6015278d7fcc4a5b7799c16ae7002bc0d8cfa949
Sachinandan <sachinandanp5@gmail.com>
2024-09-21 16:47:20 +0530

    featuredCourses ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
     <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       {/* <h1 className="text-3xl text-center">Hello, Next.js!</h1> */}
       <HeroSection />
+      <FeaturedCourses/>
 
     </main>
   );
